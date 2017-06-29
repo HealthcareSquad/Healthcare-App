dfb6efefdb7ab890619d9bba06bd7fdd09e3051c
R Matthews <remimatthews@brandeis.edu>
2017-06-29 17:26:56 -0400

    feedback included but not functional

--- server/main.js
+++ server/main.js
@@ -2,4 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+
+  //Feedback.remove({});  // clear the database
+  Feedback.insert({Name: 'Your Name',Feedback:'Feedback goes here'});
 });
