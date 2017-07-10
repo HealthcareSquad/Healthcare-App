a5be6f256a7ccf291ce35d9b17790c829fa06c5a
nilamandal <nmandal@brandeis.edu>
2017-07-10 14:28:09 -0400

    geocoding api with jquery

---
+++ pages/home/client/home.js
@@ -0,0 +1,42 @@
+Template.home.events({
+  'click button'(element,instance){
+    const place= instance.$('#find').val();
+    var address = place.replace(/ /g, '+');
+    instance.$('#find').val("");
+    const key= "AIzaSyAgcB5vtpwngFC9WQtyX7oDUtUNlR0hnQs";
+
+
+    const url= "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+key;
+    console.log(url);
+    jQuery.getJSON(url, function(data){
+
+      console.log(data.results[0].geometry.location.lat);
+      console.log(data.results[0].geometry.location.lng);
+    });
+
+
+  }
+})
+
+
+
+
+
+
+Template.results.events({
+  //this is for a insert button, change it to a span or other thing as needed
+  'click button'(elt, instance){
+    const name = instance.$('#name').val();
+    const specialized = instance.$('#specialized').val();
+    const insurance = instance.$('#insurance').val();
+    //whatever other constants we need
+    var doctor =
+      { name:name,
+        specialized:specialized,
+        insurance:insurance
+        //again, whatever other things we need
+      };
+    Meteor.call('results.insert',doctor);
+
+  }
+})
