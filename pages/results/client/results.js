263bb7ec116f7469ac2e375731d28fde81a3c764
eholderness95 <egh@brandeis.edu>
2017-07-10 11:13:53 -0400

    lil update
    bbb

--- pages/results/client/results.js
+++ pages/results/client/results.js
@@ -1,8 +1,15 @@
 Template.results.onCreated(function resultsCreated(){
   window.onload = jQuery.getJSON('https://api.betterdoctor.com/2016-03-01/doctors?location=ma-waltham&skip=0&limit=10&user_key=876a19607233e092fdc4a30a9c079614', function(data) {
-    let txt = "<table class=\"table-hover\">";
+    let txt = "<table class=\"table table-hover\">";
     for (x in data.data) {
-      txt += "<tr><td>" + data.data[x].profile.first_name + ' ' + data.data[x].profile.last_name + ', ' + data.data[x].profile.title + "</td></tr>";
+      txt += "<a href=\"docProfile.html\"" + data.data[x].uid + "\"\"><tr><td>" + data.data[x].profile.first_name + ' ' + data.data[x].profile.last_name + ', ' + data.data[x].profile.title + "</td><td>" + data.data[x].practices[0].name;
+      if (data.data[x].practices.length > 1){
+        txt += ", and " + data.data[x].practices.length + " others</td>";
+      }else{
+        txt += "</td>";
+      }
+
+      txt += "</tr></a>";
     }
     txt += "</table>";
     document.getElementById("results").innerHTML = txt;
@@ -10,11 +17,9 @@ Template.results.onCreated(function resultsCreated(){
 });
 
 Template.results.events({
-  'submit form': function(event, template) {
+  'click span': function(event, template) {
     event.preventDefault();
-    var query = template.$('input[type=text]').val();
-    if (query)
-      Session.set('query', query);
+    Router.go('docProfile docID="this.span.id"');
   }
 });
 
