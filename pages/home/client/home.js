a61e7b5003b785cb064fda2d95297dc69aa303bc
eholderness95 <egh@brandeis.edu>
2017-07-10 17:03:23 -0400

    Big functionality update
    Added working searchbar, iterative table construction, and doctor profiles

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -1,3 +1,6 @@
+var latitude;
+var longitude;
+
 Template.home.events({
   'click button'(element,instance){
     const place= instance.$('#find').val();
@@ -9,16 +12,12 @@ Template.home.events({
     const url= "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+key;
     console.log(url);
     jQuery.getJSON(url, function(data){
-      const latitude = data.results[0].geometry.location.lat;
-      const longitude = data.results[0].geometry.location.lng;
+      latitude = data.results[0].geometry.location.lat;
+      longitude = data.results[0].geometry.location.lng;
     });
+    }
 
-
-  }
-})
-
-
-
+});
 
 
 
