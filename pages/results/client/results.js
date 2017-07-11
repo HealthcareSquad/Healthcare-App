d5ff3e5bb9b94246a860bc758fa98e2b09006a00
eholderness95 <egh@brandeis.edu>
2017-07-11 13:57:46 -0400

    Doc profiles
    Version 1!

--- pages/results/client/results.js
+++ pages/results/client/results.js
@@ -1,7 +1,7 @@
 Template.results.onCreated(function resultsCreated(){
     const latitude = Router.current().params.query.lat;
     const longitude = Router.current().params.query.long;
-    jQuery.getJSON('https://api.betterdoctor.com/2016-03-01/doctors?location=' + latitude + '%2C' + longitude + '%2C100&skip=0&limit=10&user_key=876a19607233e092fdc4a30a9c079614', function(data) {
+    jQuery.getJSON('https://api.betterdoctor.com/2016-03-01/doctors?location=' + latitude + '%2C' + longitude + '%2C100&skip=0&limit=20&user_key=876a19607233e092fdc4a30a9c079614', function(data) {
       let txt = "<table class=\"table table-hover\"><thead><tr><td>Name</td><td>Location(s)</td></tr></thead><tbody data-link=\'row\' class=\'rowlink\'>";
       for (x in data.data) {
         txt += "<tr><td><a id=\'docLink\' data-uid=\'" + data.data[x].uid + "\'href=\'#docModal\' data-toggle=\'modal\'>" + data.data[x].profile.first_name + ' ' + data.data[x].profile.last_name + ', ' + data.data[x].profile.title + "</a></td><td>" + data.data[x].practices[0].name;
@@ -23,7 +23,37 @@ Template.results.events({
     event.preventDefault();
     uid = doc.target.dataset.uid;
     jQuery.getJSON('https://api.betterdoctor.com/2016-03-01/doctors/' + uid + '?user_key=876a19607233e092fdc4a30a9c079614', function(data){
-    document.getElementById("docModalName").innerHTML = "<h4>" + data.data.profile.first_name + " " + data.data.profile.last_name + "</h4><br><p>More to come!</p>";
+      document.getElementById("docModalName").innerHTML = "<p><strong>" + data.data.profile.first_name + " " + data.data.profile.last_name + ", " + data.data.profile.title + "</strong></p>";
+      document.getElementById("profImage").innerHTML = "<img src=\"" + data.data.profile.image_url + "\"><br>";
+      document.getElementById("docBio").innerHTML = "<p class=\'h5\'>" + data.data.profile.bio + "</p>";
+      let txt = "<em><p class=\"h6 text-center\">Specializes in " + data.data.specialties[0].name;
+      if (data.data.specialties.length > 1){
+        for(i=0;i<data.data.specialties.length-1;i++){
+          if (data.data.specialties[i].name != data.data.specialties[i+1].name){
+            txt += ", " + data.data.specialties[i].name;
+          }
+        }
+        txt += ", and " + data.data.specialties[data.data.specialties.length-1].name;
+      }
+      txt += "</p></em>";
+      document.getElementById("specInfo").innerHTML = txt;
+      txt = "<p>Speaks " + data.data.profile.languages[0].name;
+      if (data.data.profile.languages.length > 1){
+        for (i=0;i<data.data.profile.languages.length-1;i++){
+            txt += ", " + data.data.profile.languages[i].name;
+        }
+        txt += ", and " + data.data.profile.languages[data.data.profile.languages.length-1].name;
+      }
+      txt += "</p>";
+      document.getElementById("langSpoken").innerHTML = txt;
+      txt = "<p>Licensed to practice in: " + data.data.licenses[0].state;
+      for (i=1;i<data.data.licenses.length;i++){
+        txt += ", " + data.data.licenses[i].state;
+      }
+      txt += "</p>";
+      document.getElementById("docLicenses").innerHTML = txt;
+
+
     });
   }
 });
