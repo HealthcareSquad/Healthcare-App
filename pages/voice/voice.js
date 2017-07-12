9a0b43a23d61327a06589d8b9814491477810220
eholderness95 <egh@brandeis.edu>
2017-07-12 12:23:02 -0400

    Deleted a page
    voice page

--- pages/voice/voice.js
+++
@@ -1,9 +0,0 @@
-/**Template.voice.events({
-  'click button'(element,instance){
-      jQuery.getJSON(url, function(data){
-        response = data.results[0].fulfillment.speech;
-        console.log(response);
-      });
-  }
-});
-*/
