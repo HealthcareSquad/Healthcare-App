2063aa85607d1e5e1cc97f459f8a1cda778dc7a1
nilamandal <nmandal@brandeis.edu>
2017-07-24 14:05:49 -0400

    cleaned up

--- pages/create/client/create.js
+++ pages/create/client/create.js
@@ -13,10 +13,6 @@ Template.create.events({
     instance.$('#username').val("");
     instance.$('#password').val("");
     instance.$('#confirm').val("");
-
-
-  
-
   }
 });
 
@@ -24,41 +20,12 @@ Template.create.events({
   'click span'(element, instance) {
   console.log(this.profile._id);
   var feedID = this.profile._id
-
   Meteor.call('profiles.remove',feedID);
 },
-
- update(){//this is where everything is updated
-      if(!(instance.$('#name').val() == "")){//if the field is not empty
-        const name = instance.$('#name').val();//save the value and call the meteor update function
-        Meteor.call('profiles.name.update', name)
-        instance.$('#name').val("");//reset the input area
-      }
-      if(!(instance.$('#insurance').val() == "")){
-        const insurance = instance.$('#insurance').val();
-        Meteor.call('profiles.insurance.update', insurance)
-        instance.$('#insurace').val("");
-      }
-      if(!(instance.$('#location').val() == "")){
-        const location = instance.$('#location').val();
-        Meteor.call('profiles.location.update', location)
-        instance.$('#location').val("");
-      }
-      if(!(instance.$('#prescription').val() == "")){
-        const prescription = instance.$('#prescription').val();
-        Meteor.call('profiles.prescription.update', prescription)
-        instance.$('#prescription').val("");
-      }
-    },
   'click #login'(elt, instance){
     const username = instance.$('#username').val();
     const password = instance.$('#password').val();
     Meteor.loginWithPassword(username,password);
     console.log(username);
-
   }
-
-
-
-
 })
