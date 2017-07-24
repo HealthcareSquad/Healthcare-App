a4e2d1c0765e6094f8703e85f7db1c34aec81f30
nilamandal <nmandal@brandeis.edu>
2017-07-24 11:55:53 -0400

    "creating accounts"

--- pages/profile/client/profile.js
+++ pages/profile/client/profile.js
@@ -3,12 +3,14 @@
 //});
 Template.profiles.onCreated(function() {
   Meteor.subscribe('profiles');
+//  document.getElementById("createAccount").style.backgroundColor = "#1985A1";
+//  document.getElementById("addProfile").style.backgroundColor = "#1985A1";
 });
 
 Template.profiles.helpers({
   profileslist() {return Profiles.find()}
 })
-
+/**
 Template.addProfile.events({
   'click button'(elt,instance){
     const name = instance.$('#name').val();
@@ -29,14 +31,15 @@ Template.addProfile.events({
     console.log(Profiles.findOne({owner:Meteor.userId()}));
   }
 });
-
+*/
 Template.profiles.events({
   'click span'(element, instance) {
   console.log(this.profile._id);
   var feedID = this.profile._id
 
   Meteor.call('profiles.remove',feedID);
-},
+}
+  /**
  update(){//this is where everything is updated
       if(!(instance.$('#name').val() == "")){//if the field is not empty
         const name = instance.$('#name').val();//save the value and call the meteor update function
@@ -64,7 +67,5 @@ Template.profiles.events({
     const password = instance.$('#password').val();
     console.log(username);
 
-  }
+  }*/
 })
-
-// probably want an upsert method
