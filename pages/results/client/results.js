33dddc29cc90d16b139bd52ec1e987088711027d
eholderness95 <egh@brandeis.edu>
2017-07-30 14:24:21 -0400

    Reset meteor
    b/c of profile structure update

--- pages/results/client/results.js
+++ pages/results/client/results.js
@@ -156,8 +156,6 @@ Template.results.events({
       if (foundIt === false){
         document.getElementById(uid).style.backgroundColor = "#fc6f6f";
         list.push(doctor);
-        console.log(doctor);
-        console.log(Meteor.user());
       }
       Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.favorites": list}});
     }else{
